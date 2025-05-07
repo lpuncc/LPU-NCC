baf99428af48b89f19f2a98a9b187d557b2df9ef
lpuncc <umesh212004@gmail.com>
2025-05-07 23:15:39 +0530

    Update server.js

--- REGISTER SERVER/server.js
+++ REGISTER SERVER/server.js
@@ -95,12 +95,9 @@ app.post("/api/register", async (req, res) => {
         await newRegistration.save();
         res.json({ message: "Registration successful" });
     } catch (error) {
-    if (error.code === 11000 && error.keyPattern?.regNumber) {
-      return res.status(400).json({ message: "Registration number already exists." });
+        console.error("Error saving registration:", error);
+        res.status(500).json({ message: "Registration number already exists." });
     }
-    console.error("Error saving registration:", error);
-    res.status(500).json({ message: "Registration number already exists." });
-  }
 });
 
 
